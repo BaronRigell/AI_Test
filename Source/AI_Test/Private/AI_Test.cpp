025420e9dcc0ecf964eeb9c109603798b20f78e7
Nikita Kuznetsov <kuzgaroth7official@gmail.com>
2023-08-19 23:47:15 +0300

    Initial Commit

---
+++ Source/AI_Test/Private/AI_Test.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AI_Test/Public/AI_Test.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AI_Test, "AI_Test" );
+ 
---
+++ Source/AI_Test/Private/AI_TestGameMode.cpp
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AI_Test/Public/AI_TestGameMode.h"
+#include "AI_Test/Public/Player/AI_TestCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AAI_TestGameMode::AAI_TestGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/AI_Test/Core/Characters/Player/BP_FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+}
